6544a755c9e474d30fd1e68b95b5184e639f304a
AndriyKozh <max190716@ukr.net>
2022-12-13 01:13:18 +0200

    hw1/v1

---
+++ myNewProject/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "red", fontSize: "32" }}>React Native </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+    alignItems: "center",
+
+    justifyContent: "center",
+  },
+});
